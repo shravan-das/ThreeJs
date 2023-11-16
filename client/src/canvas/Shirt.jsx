ae5e3bdb302bb6aefa9418f8698aac0026f45d0a
Shravan <ishravan919@gmail.com>
2023-11-16 17:24:26 +0530

    small change

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -14,6 +14,7 @@ const Shirt = () => {
   const fullTexture = useTexture(snap.fullDecal);
 
   useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+  
 
   const stateString = JSON.stringify(snap);
 
